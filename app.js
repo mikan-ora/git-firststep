43c95d8403739d94b7f65319235835bbbaae74a1
ishikawa <ishikawa.mi.mobile@gmail.com>
2020-05-04 13:35:22 +0900

    Add WEBサイトデータ

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('Hello ProtoOut!');
+// axiosライブラリを呼び出す
+const axios = require('axios');
+
+// 実際にデータを取得する getRequest 関数
+async function getRequest() {
+  try {
+    const response = await axios.get(`https://mikan-ora.github.io/git-firststep/data.json`);
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// getRequest を呼び出してデータを読み込む
+getRequest();
