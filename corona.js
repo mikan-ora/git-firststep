7f72d6c4ed11d18480423a6a027a937139c17409
ishikawa <ishikawa.mi.mobile@gmail.com>
2020-05-05 15:35:01 +0900

    Add WEBサイトデータ

--- corona.js
+++ corona.js
@@ -36,7 +36,7 @@ async function getRequest() {
     //  msg: unko,
     //};
 
-    //await updateData(saveData); //データ更新関数を実行
+   await updateData(unko); //データ更新関数を実行
   } catch (error) {
     console.error(error);
   }
